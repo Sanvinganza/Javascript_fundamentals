619ca3a0ea64c22606643bd78185bedaa2e992e3
Anton Shvetsov <anton.shvetsov@mail.ru>
2022-02-21 17:00:23 +0300

    FIX: fix function skip

--- ArrayLibrary/skip.js
+++ ArrayLibrary/skip.js
@@ -2,6 +2,8 @@ export function mySkip(array, n) {
   let result = [];
   let i = 0;
 
+  n--; // because array start with 0
+
   for (; i < array.length; i++) {
     if (i !== n) {
       result.push(array[i]);
