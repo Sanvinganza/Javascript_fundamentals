3ffa0aa3f5f7ef2cec400c33998470d502e2cdbe
Anton Shvetsov <anton.shvetsov@mail.ru>
2022-02-22 15:33:11 +0300

    changed root object and chain function

--- ArrayLibrary/index.js
+++ ArrayLibrary/index.js
@@ -6,70 +6,71 @@ import { myTake } from "./take";
 import { myForEach } from "./foreach";
 
 export function ARRAY_LIB() {
-  let _array;
-  
-  this.filter = function(array, callback) {
-    // if the first parameter is not an array, 
-    // then an argument shift has occurred
-    if(!Array.isArray(array)) {
-      _array = myFilter(_array, array);
+  this._array;
+  var self = this;
+
+  this.filter = function (array, callback) {
+    if (self._array) {
+      self._array = myFilter(self._array, array);
       return this;
     }
-    
+
     return myFilter(array, callback);
   };
-  
-  this.map = function(array, callback) {
-    if(!Array.isArray(array)) {
-      _array = myMap(_array, array);
+
+  this.map = function (array, callback) {
+    if (self._array) {
+      self._array = myMap(self._array, array);
       return this;
     }
-    
+
     return myMap(array, callback);
   };
-  
-  this.reduce = function(array, callback, initialValue) {
-    if(!Array.isArray(array)) {
-      _array = myReduce(_array, array, callback);
+
+  this.reduce = function (array, callback, initialValue) {
+    if (self._array) {
+      self._array = myReduce(self._array, array, callback);
       return this;
     }
-    
+
     return myReduce(array, callback, initialValue);
   };
-  
-  this.take = function(array, n) {
-    if(!Array.isArray(array)) {
-      _array = myTake(_array, array);
+
+  this.take = function (array, n) {
+    if (self._array) {
+      self._array = myTake(self._array, array);
       return this;
     }
-    
+
     return myTake(array, n);
   };
-   
-  this.skip = function(array, n) {
-    if(!Array.isArray(array)) {
-      _array = mySkip(_array, array);
+
+  this.skip = function (array, n) {
+    if (self._array) {
+      self._array = mySkip(self._array, array);
       return this;
     }
-    
+
     return mySkip(array, n);
   };
 
-  this.foreach = function(array, callback) {
-    if(!Array.isArray(array)) {
-      _array = myForEach(_array, array);
+  this.foreach = function (array, callback) {
+    if (self._array) {
+      self._array = myForEach(self._array, array);
       return this;
     }
-    
-    _array = myForEach(array, callback);
+
+    myForEach(array, callback);
   };
-  
-  this.chain = function(array) {
-    _array = array;
+
+  this.chain = function (array) {
+    self._array = array;
     return this;
-  }
-  
-  this.value = function() {
-    return _array;
-  }
+  };
+
+  this.value = function () {
+    const _ = self._array;
+    delete self._array;
+    return _;
+  };
 }
